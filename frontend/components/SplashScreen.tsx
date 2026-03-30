b159184a0eb20186f9f28e1caf3073648471dc65
Divyanshu-chauhan000 <divyanshuchauhan15072003@gmail.com>
2026-03-30 18:25:45 +0530

    Edit splash screen

--- frontend/components/SplashScreen.tsx
+++ frontend/components/SplashScreen.tsx
@@ -3,22 +3,30 @@ import { Animated, Image, Text, View } from 'react-native'
 
 export const SplashScreen = ({onFinish} : any) => {
   const translateX = useRef(new Animated.Value(-200)).current;
+   const textAnim = useRef(new Animated.Value(200)).current;
   const opacity = useRef(new Animated.Value(0)).current;
 
  useEffect(() =>{
   Animated.parallel([
     Animated.timing(translateX,{
       toValue : 0,
-      duration: 1200,
+      duration: 1000,
       useNativeDriver: true
     }),
     Animated.timing(opacity, {
       toValue : 1,
-      duration : 1200,
+      duration : 1000,
+      useNativeDriver: true
+    }),
+      Animated.timing(textAnim,{
+      toValue : 0,
+      duration: 1000,
       useNativeDriver: true
     }),
   ]).start(() =>{
-  onFinish ();
+  setTimeout(() => {
+        onFinish();
+      }, 500);
   })
  }, [])
 
@@ -26,15 +34,42 @@ export const SplashScreen = ({onFinish} : any) => {
   return (
   <View className='flex-1 bg-[#2b0a0a] justify-center items-center'>
 
-    <Animated.View style={{transform : [{translateX}] , opacity}}   className="flex-row items-center">
-      <Image source={require("../assets/LoginImages/hat_7270962.png")} className='w-14 h-14 mr-3 ' />
-    <Text className='text-white text-3xl font-bold'>QuickRecipe</Text>
-    </Animated.View>
-    <Animated.View
+      <View className="flex-row items-center">
+
+        {/* Image */}
+        <Animated.Image
+          source={require("../assets/LoginImages/hat_7270962.png")}
+          style={{
+            width: 50,
+            height: 50,
+            marginRight: 10,
+            transform: [{ translateX: translateX }],
+            opacity
+          }}
+        />
+
+        {/* Text */}
+        <Animated.Text
+          style={{
+            fontSize: 28,
+            fontWeight: 'bold',
+            color: 'white',
+            transform: [{ translateX: textAnim }],
+            opacity
+          }}
+        >
+          QuickRecipe
+        </Animated.Text>
+
+      </View>
+
+      {/* Bottom loader bar */}
+      <Animated.View
         style={{ opacity }}
-        className="mt-3 w-16 h-4 bg-white/20 rounded-full"
+        className="mt-4 w-20 h-2 bg-white/20 rounded-full"
       />
-  </View>
+
+    </View>
   )
 }
 
